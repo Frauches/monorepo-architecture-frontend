371401116b89282164ad08566c242359c1cf6ebd
Matheus Frauches <frauches.27@gmail.com>
2024-03-04 22:00:49 -0300

    Implements utils package and setup its workspace

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import { sum } from "@frauches/utils/math";
 
 export default function HomeScreen() {
   return (
     <main>
       <h1> Home </h1>
+
+      <p>
+        It has to sum numbers using the function from utils package workspace{" "}
+        {sum(5, 3)}
+      </p>
     </main>
   );
 }
